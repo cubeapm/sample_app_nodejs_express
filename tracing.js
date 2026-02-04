4fdb234babf0601dfaa0b588c76b378191b306c1
Vijay Aggarwal <aggarwalvijay007@gmail.com>
2026-02-04 21:06:12 +0530

    added log decoration

--- tracing.js
+++ tracing.js
@@ -8,6 +8,12 @@ const {
 const {
   OTLPTraceExporter,
 } = require("@opentelemetry/exporter-trace-otlp-proto");
+const {
+    BatchLogRecordProcessor,
+    SimpleLogRecordProcessor,
+    ConsoleLogRecordExporter,
+} = require('@opentelemetry/sdk-logs');
+const { OTLPLogExporter } = require("@opentelemetry/exporter-logs-otlp-proto");
 
 if (process.env.OTEL_LOG_LEVEL === "debug") {
   diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
@@ -21,11 +27,22 @@ const traceExporter =
     ? new opentelemetry.tracing.ConsoleSpanExporter()
     : new OTLPTraceExporter(exporterOptions);
 
+const logProcessor = process.env.OTEL_LOG_LEVEL === "debug"
+    ? new SimpleLogRecordProcessor(new ConsoleLogRecordExporter())
+    // It automatically reads ENDPOINT and HEADERS from env variables
+    : new BatchLogRecordProcessor(new OTLPLogExporter({}));
+
 const sdk = new opentelemetry.NodeSDK({
   traceExporter,
+  logRecordProcessors: [logProcessor],
   instrumentations: getNodeAutoInstrumentations({
     "@opentelemetry/instrumentation-fs": {
       enabled: false,
+    },
+    "@opentelemetry/instrumentation-winston": {
+      // Log sending needs @opentelemetry/winston-transport npm package.
+      // If it is not installed, logs will not be sent.
+      disableLogSending: !process.env.OTEL_EXPORTER_OTLP_LOGS_ENDPOINT,
     },
   }),
 });
